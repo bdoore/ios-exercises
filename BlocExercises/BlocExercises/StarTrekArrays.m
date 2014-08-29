8c8cb974d9753c54b170e26bbfcaf530bd192d36
Brian Doore <doore.b@gmail.com>
2014-08-29 11:58:56 -0500

    Completed arrays

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -11,23 +11,31 @@
 @implementation StarTrekArrays
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
-    /* WORK HERE */
-    return @[];
+    NSArray *output = [characterString componentsSeparatedByString:@";"];
+    return output;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @"";
+    NSString *output =[characterArray componentsJoinedByString:@";"];
+    return output;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @[];
+    NSMutableArray *sorted = [characterArray mutableCopy];
+    NSSortDescriptor *sortDescriptor =[[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    [sorted sortUsingDescriptors:@[sortDescriptor]];
+    return sorted;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
-    /* WORK HERE */
-    return NO;
+    
+    BOOL temp = NO;
+    
+    for(int i=0; i < [characterArray count]; i++){
+        if ([characterArray[i] isEqualToString:(@"Worf, son of Mogh, slayer of Gowron")])
+            temp = YES;
+    }
+    return temp;
 }
 
 @end
