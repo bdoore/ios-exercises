f962fa2e06550648bdb8805f087569455f5acf65
Brian Doore <doore.b@gmail.com>
2014-09-01 14:22:01 -0500

    Completed dictionaries

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -11,18 +11,31 @@
 @implementation StarTrekDictionaries
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
-    return @"";
+    NSString *temp = characterDictionary[@"favorite drink"];
+    return temp;
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
-    /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *temp = [[NSMutableArray alloc] init];
+    
+    for(NSInteger i = 0; i < charactersArray.count; i++) {
+        
+        [temp addObject: [charactersArray objectAtIndex:(i)][@"favorite drink"]];
+        /*
+        NSDictionary *character = [charactersArray objectAtIndex:(i)];
+        [temp addObject: character[@"favorite drink"]];
+         */
+    }
+        
+    return temp;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
-    return @{};
+    
+    NSMutableDictionary *temp = [characterDictionary mutableCopy];
+    [temp setObject:@"Make it so" forKey:@"quote"];
+    return temp;
 }
 
 @end
